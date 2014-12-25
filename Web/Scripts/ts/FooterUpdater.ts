6b664ea5d1e0ce3f2423e1c0e0d67d8384d106a1
Ahmed ElSayed <ahmels@microsoft.com>
2014-12-24 21:51:27 -0800

    Adding some TypeScript 1.3 language features

--- Web/Scripts/ts/FooterUpdater.ts
+++ Web/Scripts/ts/FooterUpdater.ts
@@ -1,9 +1,24 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
 
 module FooterUpdater {
-   export function SendMessageFromTypeScript() {
-      var theMessage = "Hello, TypeScript Footer!";
-      $("#message-from-typescript").text(theMessage);
+    class MessageCreater {
+        constructor() {
+            console.log("getting constructed");
+        }
+
+        public getMessage(): string {
+            return "Hello, TypeScript Footer!";
+        }
+
+        protected protectedFunction() {
+            console.log("protected function");
+        }
+    }
+    export function SendMessageFromTypeScript() {
+        var tuple: [number, string] = [1, "string"];
+        var msgCreator = new MessageCreater();
+        tuple[1] = msgCreator.getMessage();
+      $("#message-from-typescript").text(tuple[1]);
    }
 
    SendMessageFromTypeScript();
